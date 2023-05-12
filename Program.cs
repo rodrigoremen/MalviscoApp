d023ddbe55273c48422c977c7d524aad6ea941be
Rodrigo Dorantes <106606020+rodrigoremen@users.noreply.github.com>
2023-05-12 13:48:16 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Hello
+{
+    static void Main()
+   
+    {
+
+        bool bandera = true;
+
+        Console.WriteLine("Hola, ingresa el numero de paquetes");
+        do
+        {
+             if(!int.TryParse(Console.ReadLine(), out int num))
+        {
+            Console.WriteLine("Se pide que sean numeros enteros, intentalo de nuevo");
+            bandera = true;
+        }
+        else
+        {
+            Console.WriteLine("El numero de cajas es: "+ num/6 + " y el numero de restantes es: "+ num % 6);
+            bandera = false;
+        }
+        } while (bandera);
+       
+    }
+}
